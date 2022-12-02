a605436117f7f6a5a69d577da5e3a0bd6fcd8c03
R3X <andruvargas023@gmail.com>
2022-12-02 16:59:33 -0600

    carpeta js

---
+++ js/calculo_consumo.js
@@ -0,0 +1,13 @@
+    var tanque = 40;
+    var tanqueGasolina = 480;
+    var tanqueAlcohol = 300;
+
+    var eficienciaGasolina = tanqueGasolina / tanque;
+    var eficienciaAlcohol = tanqueAlcohol / tanque;
+
+    document.write("La eficiencia del carro utilizando gasolina es de " + eficienciaGasolina + " km/L")
+
+    document.write("<br>")
+    document.write("<br>")
+
+    document.write("La eficiencia del carro utilizando alcohol es de " + eficienciaAlcohol + " km/L")
